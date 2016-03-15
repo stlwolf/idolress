c6c333e74a5ccffaab5aec3e7c7bdc76b9cf2eea
eddy <eddy@stlwolf.com>
2016-03-16 08:13:58 +0900

    add http func

--- idolrss/ViewController.swift
+++ idolrss/ViewController.swift
@@ -16,6 +16,61 @@ import HTMLReader
 import PullToRefreshSwift
 import SwiftyJSON
 
+// RSSのJSONをパースする
+func parse(url: String, completion: (([JSON]?) -> Void)) {
+    
+    let url = NSURL(string: url)
+    
+    // リクエスト送信
+    Alamofire.request(.GET, url!, parameters: nil, encoding: .JSON).responseJSON { response in
+        guard let object = response.result.value else {
+            return
+        }
+        
+        // entryの取り出し
+        let json = JSON(object)
+        let entries = json["responseData"]["feed"]["entries"].array
+        
+        completion(entries)
+    }
+}
+
+// 指定URLのHTMLと画像を取得
+func getContents(url: String, completion: ((AnyObject) -> Void)) {
+    
+    let url = NSURL(string: url)
+    var result = [String: String!]()
+    
+    Alamofire.request(.GET, url!, parameters: nil).responseString { response in
+        guard let object = response.result.value else {
+            return
+        }
+        
+        var content = ""
+        let html = HTMLDocument(string: object)
+        
+        let ogTags = html.nodesMatchingSelector("meta[property=\"og:description\"]")
+        if ogTags.isEmpty {
+            for tag in ogTags {
+                content = (tag.attributes?["content"] as? String)!
+            }
+        }
+        
+        var image = ""
+        let imgTags = html.nodesMatchingSelector("img")
+        if imgTags.isEmpty {
+            for tag in imgTags {
+                if let data = tag.attributes?["data-src"] {
+                    image = data as! String
+                }
+            }
+        }
+        
+        result = ["content": content, "image": image]
+        completion(result)
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
