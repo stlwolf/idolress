82298c9258acc21006f1a6e7c3a244ed0d9c0c42
eddy <eda@mynet.co.jp>
2016-03-18 14:01:44 +0900

    update viewcontroller.swift

--- idolrss/ViewController.swift
+++ idolrss/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController, CAPSPageMenuDelegate {
         
         self.title = "IDOL Reeder"
         var controllerArray = [UIViewController]()
-        var feedArray: [Dictionary<String, String!>] =
+        let feedArray: [Dictionary<String, String!>] =
         [
             [
                 "link" : "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&q=http://rss.dailynews.yahoo.co.jp/fc/computer/rss.xml&num=10" ,
@@ -39,16 +39,43 @@ class ViewController: UIViewController, CAPSPageMenuDelegate {
             ]
         ]
         
+        // 記事取得
         for feed in feedArray {
             
+            let feedViewController = FeedTableViewController()
+            
+            // 記事情報
+            feedViewController.link = feed["link"]!
+            feedViewController.title = feed["title"]!
+            
+            // 親controllerはこいつ自身
+            feedViewController.parent = self
+            
+            // PageMenu用にviewController配列に突っ込む
+            controllerArray.append(feedViewController)
         }
+        
+        // PageMenu設定
+        let parameters: [String : AnyObject] = [
+            "menuItemSeparatorWidth": 4.3,
+            "useMenuLikeSegmentedControl": true,
+            "menuItemSeparatorPercentageHeight": 0.1,
+            "scrollMenuBackgroundColor": UIColor.grayColor(),
+            "selectionIndicatorColor": UIColor.blackColor(),
+        ]
+        
+        let statusBarHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.height
+        let naviHeight: CGFloat = self.navigationController!.navigationBar.frame.size.height
+        let yPosition = naviHeight + statusBarHeight
+        
+        // 上部に作成するページメニューライブラリの作成
+        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, yPosition, self.view.frame.width, self.view.frame.height - yPosition), options: parameters)
+        self.view.addSubview(pageMenu!.view)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
